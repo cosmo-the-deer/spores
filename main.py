8517bbf07ed9f4cf14ee5069ec92ae8a030ae1ef
cosmo_the_deer <cosmothedeer@gmail.com>
2025-05-22 19:51:24 +1200

    added buildings

--- main.py
+++ main.py
@@ -20,70 +20,69 @@ spores_bonus_harvest = 0
 spores_per_multiplyer = 1
 new_game = True
 farm_name = ''
+buildings = ['crop', 'scare_crow']
+building_give_amount = {'crop': 1, 'scare_crow': 10}
+building_prices = {'crop': 10, 'scare_crow': 100}
+building_amounts = {'crop': 1, 'scare_crow': 1}
 
 
-# if you are editing this to make it better please find a way to reload instead of exiting
-# this resets the var and saves it to the data.txt file
-def start_new_game():
-    global spores, spores_per_harvest, spores_bonus_harvest, spores_per_multiplyer, new_game
-    spores = 0
-    spores_per_harvest = 1
+def calculate_harvest():
+    global spores_bonus_harvest, spores_per_multiplyer, spores_per_harvest, building_amounts, buildings, building_give_amount
+    spores_per_harvest = 0
     spores_bonus_harvest = 0
     spores_per_multiplyer = 1
+    for i in range(len(buildings)):
+        spores_per_harvest += building_amounts[buildings[i]] * building_give_amount[buildings[i]]
+
+def start_new_game():
+# if you are editing this to make it better please find a way to reload instead of exiting
+# this resets the var and saves it to the data.txt file
+    global spores, spores_per_harvest, spores_bonus_harvest, spores_per_multiplyer, new_game
+    spores = 0
     farm_name = 'dr mylo\'s farm'
     new_game = True
     save()
     exit()
 
-
+def load():
 # this sets the variables to the values in the data.txt file
 # it can be called at the start of the game to load the game
 # these are global variables so they can be used in the functions
-def load():
     global spores, spores_per_harvest, spores_bonus_harvest, spores_per_multiplyer, new_game, farm_name
     file = open('data.txt', 'r')
     data = file.readlines()
     file.close()
     spores = int(data[0].replace('spores:', '').strip())
-    spores_per_harvest = int(data[1].replace('spores per harvest:', '').strip())
-    spores_bonus_harvest = int(data[2].replace('spores bonus harvest:', '').strip())
-    spores_per_multiplyer = int(data[3].replace('spores per multiplyer:', '').strip())
-    farm_name = data[4].replace('farm name:', '').strip()
-    new_game = data[5].replace('new game:', '').strip() == '1'
+    farm_name = data[1].replace('farm name:', '').strip()
+    new_game = data[2].replace('new game:', '').strip() == '1'
 
-
+def save():
 # this function is used to save the game data to the data.txt file
 # it is called when the game is exited or when the player wants to save the game
 # it uses the global variables to save the data
 # it overwrites the data.txt file with the new data
 # it is called in the new_game function.
-def save():
     global spores, spores_per_harvest, spores_bonus_harvest, spores_per_multiplyer, new_game
     file = open('data.txt', 'w')
     file.write('spores: ' + str(spores) + '\n')
-    file.write('spores per harvest: ' + str(spores_per_harvest) + '\n')
-    file.write('spores bonus harvest: ' + str(spores_bonus_harvest) + '\n')
-    file.write('spores per multiplyer: ' + str(spores_per_multiplyer) + '\n')
     file.write("farm name: " + farm_name + '\n')
     file.write('new game: ' + ('1' if new_game else '0')  + '\n')
     file.close()
     print('game saved successfully')
 
-
+def print_file(file):
 # this function is used to print.txt files and close them
 # it is used in the help command and the license command
-def print_file(file):
     with open(file, 'r') as f:
         print(f.read())
 
-
+def intro():
 # this fuction is used to print the intro to the game
 # and check if the player has a new game or not
 # if the player has a new game it will ask for the name of the farm
 # and save it to the data.txt file
 # if the player does not have a new game it will print the name of the farm
 # it is called at the start of the game
-def intro():
     global new_game, farm_name
     print('welcome to spores.')
     if new_game == True:
@@ -120,31 +119,29 @@ def clear_screen():
         print('your system type cannot be cleared')
 
 def harvest():
-    #harvests the spores and adds them to the total
+#this harvests the spores
     global spores, spores_bonus_harvest, spores_per_harvest, spores_per_multiplyer
+    calculate_harvest()
     spores_harvested = spores_per_harvest + spores_bonus_harvest * spores_per_multiplyer
     spores = spores + spores_per_harvest + spores_bonus_harvest * spores_per_multiplyer
     print('harvested spores: ' + str(spores_harvested) + '. total spores: ' + str(spores))
 
-
+def info():
 # this function is used to print the info about the current game
 # nts create a func to print the info about the saved game
 # this cannot be in a txt file because it is changing
-def info():
     print('spores: ' + str(spores))
     print('spores per harvest: ' + str(spores_per_harvest))
     print('spores per harvest bonus: ' + str(spores_bonus_harvest))
     print('spores multiplyer: ' + str(spores_per_multiplyer))
     print('farm name: ' + farm_name)
-    print('new game: ' + str(new_game))
 
-
+def main():
 #gets the players input and stores it in the variable "command"
 #then checks i it matches any of the commands
 #if it does, it runs the function associated with that command
 #if it does not, it prints an error message
 #to add a new command, add it to the list of commands useing elif, and create a function for it
-def main():
     while True:
         command = input('$=').lower()
         print()
